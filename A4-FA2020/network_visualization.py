4633e00cfcd4fde91083ecc2e2f7ea6d66425590
mmahdin <mahdi9roozi@gmail.com>
2024-08-27 16:05:02 +0330

    Complete network_visualization

--- A4-FA2020/network_visualization.py
+++ A4-FA2020/network_visualization.py
@@ -15,96 +15,120 @@ from a4_helper import *
 
 
 def hello():
-  """
-  This is a sample function that we will try to import and run to ensure that
-  our environment is correctly set up on Google Colab.
-  """
-  print('Hello from network_visualization.py!')
+    """
+    This is a sample function that we will try to import and run to ensure that
+    our environment is correctly set up on Google Colab.
+    """
+    print('Hello from network_visualization.py!')
+
 
 def compute_saliency_maps(X, y, model):
-  """
-  Compute a class saliency map using the model for images X and labels y.
+    """
+    Compute a class saliency map using the model for images X and labels y.
 
-  Input:
-  - X: Input images; Tensor of shape (N, 3, H, W)
-  - y: Labels for X; LongTensor of shape (N,)
-  - model: A pretrained CNN that will be used to compute the saliency map.
+    Input:
+    - X: Input images; Tensor of shape (N, 3, H, W)
+    - y: Labels for X; LongTensor of shape (N,)
+    - model: A pretrained CNN that will be used to compute the saliency map.
 
-  Returns:
-  - saliency: A Tensor of shape (N, H, W) giving the saliency maps for the input
-  images.
-  """
-  # Make input tensor require gradient
-  X.requires_grad_()
-  
-  saliency = None
-  ##############################################################################
-  # TODO: Implement this function. Perform a forward and backward pass through #
-  # the model to compute the gradient of the correct class score with respect  #
-  # to each input image. You first want to compute the loss over the correct   #
-  # scores (we'll combine losses across a batch by summing), and then compute  #
-  # the gradients with a backward pass.                                        #
-  # Hint: X.grad.data stores the gradients                                     #
-  ##############################################################################
-  # Replace "pass" statement with your code
-  pass
-  ##############################################################################
-  #               END OF YOUR CODE                                             #
-  ##############################################################################
-  return saliency
+    Returns:
+    - saliency: A Tensor of shape (N, H, W) giving the saliency maps for the input
+    images.
+    """
+    # Make input tensor require gradient
+    X.requires_grad_()
+
+    saliency = None
+    ##############################################################################
+    # TODO: Implement this function. Perform a forward and backward pass through #
+    # the model to compute the gradient of the correct class score with respect  #
+    # to each input image. You first want to compute the loss over the correct   #
+    # scores (we'll combine losses across a batch by summing), and then compute  #
+    # the gradients with a backward pass.                                        #
+    # Hint: X.grad.data stores the gradients                                     #
+    ##############################################################################
+    # Replace "pass" statement with your code
+    scores = model(X)
+    # gather only correct class
+    correct_scores = torch.gather(scores, 1, y.view(-1, 1))
+    loss = torch.sum(correct_scores)
+
+    loss.backward()
+    saliency = X.grad.data
+    saliency = torch.abs(saliency)
+    saliency, _ = torch.max(saliency, dim=1)
+    ##############################################################################
+    #               END OF YOUR CODE                                             #
+    ##############################################################################
+    return saliency
+
 
 def make_adversarial_attack(X, target_y, model, max_iter=100, verbose=True):
-  """
-  Generate an adversarial attack that is close to X, but that the model classifies
-  as target_y.
+    """
+    Generate an adversarial attack that is close to X, but that the model classifies
+    as target_y.
 
-  Inputs:
-  - X: Input image; Tensor of shape (1, 3, 224, 224)
-  - target_y: An integer in the range [0, 1000)
-  - model: A pretrained CNN
-  - max_iter: Upper bound on number of iteration to perform
-  - verbose: If True, it prints the pogress (you can use this flag for debugging)
+    Inputs:
+    - X: Input image; Tensor of shape (1, 3, 224, 224)
+    - target_y: An integer in the range [0, 1000)
+    - model: A pretrained CNN
+    - max_iter: Upper bound on number of iteration to perform
+    - verbose: If True, it prints the pogress (you can use this flag for debugging)
 
-  Returns:
-  - X_adv: An image that is close to X, but that is classifed as target_y
-  by the model.
-  """
-  # Initialize our adversarial attack to the input image, and make it require gradient
-  X_adv = X.clone()
-  X_adv = X_adv.requires_grad_()
-  
-  learning_rate = 1
-  ##############################################################################
-  # TODO: Generate an adversarial attack X_adv that the model will classify    #
-  # as the class target_y. You should perform gradient ascent on the score     #
-  # of the target class, stopping when the model is fooled.                    #
-  # When computing an update step, first normalize the gradient:               #
-  #   dX = learning_rate * g / ||g||_2                                         #
-  #                                                                            #
-  # You should write a training loop.                                          #
-  #                                                                            #
-  # HINT: For most examples, you should be able to generate an adversarial     #
-  # attack in fewer than 100 iterations of gradient ascent.                    #
-  # You can print your progress over iterations to check your algorithm.       #
-  ##############################################################################
-  # Replace "pass" statement with your code
-  pass
-  ##############################################################################
-  #                             END OF YOUR CODE                               #
-  ##############################################################################
-  return X_adv
+    Returns:
+    - X_adv: An image that is close to X, but that is classifed as target_y
+    by the model.
+    """
+    # Initialize our adversarial attack to the input image, and make it require gradient
+    X_adv = X.clone()
+    X_adv = X_adv.requires_grad_()
+
+    learning_rate = 1
+    ##############################################################################
+    # TODO: Generate an adversarial attack X_adv that the model will classify    #
+    # as the class target_y. You should perform gradient ascent on the score     #
+    # of the target class, stopping when the model is fooled.                    #
+    # When computing an update step, first normalize the gradient:               #
+    #   dX = learning_rate * g / ||g||_2                                         #
+    #                                                                            #
+    # You should write a training loop.                                          #
+    #                                                                            #
+    # HINT: For most examples, you should be able to generate an adversarial     #
+    # attack in fewer than 100 iterations of gradient ascent.                    #
+    # You can print your progress over iterations to check your algorithm.       #
+    ##############################################################################
+    # Replace "pass" statement with your code
+    iteration = 1
+    while True:
+        scores = model(X_adv)  # N * num_class
+        idx = torch.argmax(scores, dim=1)
+        if idx == target_y:
+            break
+        scores[:, target_y].backward()
+        grad = X_adv.grad.data
+        dX_adv = grad * learning_rate / torch.norm(grad, 2)
+        X_adv.data += dX_adv.data  # to avoid inplace
+        X_adv.grad.data.zero_()
+
+        print('Iteration %d: target score %.3f, max score %.3f' %
+              (iteration, scores[:, target_y], scores[:, idx]))
+        iteration += 1
+    ##############################################################################
+    #                             END OF YOUR CODE                               #
+    ##############################################################################
+    return X_adv
 
 
 def class_visualization_step(img, target_y, model, **kwargs):
     """
     Performs gradient step update to generate an image that maximizes the 
     score of target_y under a pretrained model.
-  
+
     Inputs:
     - img: random image with jittering as a PyTorch tensor  
     - target_y: Integer in the range [0, 1000) giving the index of the class
     - model: A pretrained CNN that will be used to generate the image
-    
+
     Keyword arguments:
     - l2_reg: Strength of L2 regularization on the image
     - learning_rate: How big of a step to take
@@ -123,7 +147,13 @@ def class_visualization_step(img, target_y, model, **kwargs):
     # after each step.                                                     #
     ########################################################################
     # Replace "pass" statement with your code
-    pass
+    scores = model(img)
+    objective = scores[:, target_y] - (l2_reg * torch.norm(img, p='fro'))
+    objective.backward()
+    grad = img.grad.data
+    dimg = learning_rate * grad / torch.norm(grad, 2)
+    img.data += dimg.data
+    img.grad.data.zero_()
     ########################################################################
     #                             END OF YOUR CODE                         #
     ########################################################################
